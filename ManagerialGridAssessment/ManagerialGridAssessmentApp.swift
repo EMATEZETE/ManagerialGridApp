7e7224c543ad006c11c0cfbc806609447ddbbb00
EMATEZETE <43090813+EMATEZETE@users.noreply.github.com>
2026-03-02 12:55:06 +0100

    Initial Commit

---
+++ ManagerialGridAssessment/ManagerialGridAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagerialGridAssessmentApp.swift
+//  ManagerialGridAssessment
+//
+//  Created by Matthias Pröschel on 02.03.26.
+//
+
+import SwiftUI
+
+@main
+struct ManagerialGridAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
